fa94d252fa9086e940fea4bcfbf879ec39de35a2
Xinhao Liang <xinhao128@hotmail.com>
2019-09-25 16:08:30 -0700

    Minor styling tweaks; remove ticket Complexity

--- src/components/NoticeboardItem.js
+++ src/components/NoticeboardItem.js
@@ -4,19 +4,13 @@ import {
     ListGroupItem
 } from 'reactstrap';
 import './NoticeboardItem.css';
+import moment from 'moment';
 
 export default function NoticeboardItem ({ticket}) {
 
     const parseDates = (UTCDate) => {
-        const date = new Date(UTCDate)
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1;
-        const formattedMonth = month < 10 ? "0" + month : month;
-        const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
-        const hours = ((date.getHours() + 11) % 12 + 1);
-        const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-        const suffix = date.getHours() > 12 ? "PM" : "AM";
-        return year + "-" + formattedMonth + "-" + day + " " + hours + ":" + minutes + " " + suffix;
+        let m = moment(UTCDate).format('llll')
+        return m;
     }
 
     const displayContent = (desp_text) => {
@@ -61,45 +55,38 @@ export default function NoticeboardItem ({ticket}) {
     }
 
     const displayPriority = (priority) => {
-        let pri_text = "";
         switch(priority) {
-            case 1:
-                pri_text = "LOW";
-                break;
             case 2:
-                pri_text = "MEDIUM";
-                break;
+                return (
+                  <div className="fr-priority medium"></div>
+                );
             case 3:
-                pri_text = "HIGH";
-                break;
+                return (
+                  <div className="fr-priority high"></div>
+                );
             default:
-                pri_text = "None";
-        }
-        return (<div className="fr-ticket-property">{pri_text}</div>);
-    }
-
-    const displayComplexity = (complexity) => {
-        if (complexity === null) {
-            return (<div className="fr-ticket-property">None</div>);
-        } else {
-            return (<div className="fr-ticket-property">{complexity}</div>);
+                return (
+                  <div className="fr-priority low"></div>
+                );
         }
     }
-    // console.log(ticket);
 
     return (
-        <ListGroupItem className="freshdesk-list" key={ticket.id}>
+        <ListGroupItem className="freshdesk-list fr-ticket-item" key={ticket.id}>
+            {displayPriority(ticket.priority)}
             <Row>
                 <Col xs="8">
-                    <Row>
-                        <Col xs="7"><div className="fr-requester">{ticket.requester.name}</div></Col>
-                        <Col xs="5"><div className="fr-date">{parseDates(ticket.created_at)}</div></Col>
-                    </Row>
                     <div className="fr-title">{ticket.subject}</div>
                     <div className="fr-description">{displayContent(ticket.description_text)}</div>
                 </Col>
-                <Col xs="4" className="right-col">
-                    <Row className="right-row">
+                <Col xs="4">
+                    <Row>
+                        <Col xs="12"><div className="fr-requester">{ticket.requester.name}</div></Col>
+                    </Row>
+                    <Row>
+                        <Col xs="12"><div className="fr-date">{parseDates(ticket.created_at)}</div></Col>
+                    </Row>
+                    <Row style={{padding: "0.5rem 0 0.3rem 0"}}>
                         <Col xs="4">
                             Tags:
                         </Col>
@@ -107,28 +94,12 @@ export default function NoticeboardItem ({ticket}) {
                             {displayTags(ticket.tags)}
                         </Col>
                     </Row>
-                    <Row className="right-row">
+                    <Row>
                         <Col xs="4">
                             Status:
                         </Col>
                         <Col xs="8">
                             {displayStatus(ticket.status)}
-                        </Col>
-                    </Row>
-                    <Row className="right-row">
-                        <Col xs="4">
-                            Priority:
-                        </Col>
-                        <Col xs="8">
-                            {displayPriority(ticket.priority)}
-                        </Col>
-                    </Row>
-                    <Row className="right-row">
-                        <Col xs="5">
-                            Complexity:
-                        </Col>
-                        <Col xs="7">
-                            {displayComplexity(ticket.custom_fields.cf_complexity)}
                         </Col>
                     </Row>
                 </Col>
