1584c3a5e041d3e38113b27d40f1ed62a9d7b873
Xinhao Liang <xinhao128@hotmail.com>
2019-09-25 16:22:55 -0700

    Rebuild website

---
+++ precache-manifest.1923d91259da4458a298d0662ba8afc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004401932b30e64a7fb1b9d7dce18f20",
+    "url": "/notice-board-freshdesk/index.html"
+  },
+  {
+    "revision": "4687d962a5f1d2afdacd",
+    "url": "/notice-board-freshdesk/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9425e101f12e9c76a159",
+    "url": "/notice-board-freshdesk/static/css/main.b0175547.chunk.css"
+  },
+  {
+    "revision": "4687d962a5f1d2afdacd",
+    "url": "/notice-board-freshdesk/static/js/2.af532fd8.chunk.js"
+  },
+  {
+    "revision": "9425e101f12e9c76a159",
+    "url": "/notice-board-freshdesk/static/js/main.78f89a86.chunk.js"
+  },
+  {
+    "revision": "dc3dc2b74a2a7b6576df",
+    "url": "/notice-board-freshdesk/static/js/runtime~main.27708c5e.js"
+  }
+]);
